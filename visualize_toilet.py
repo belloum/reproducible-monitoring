ff3694abd4f65fad51cbaac9e4363aa27565fa57
BELLOUM Rafik <rafik.belloum@inria.fr>
2020-02-10 17:15:26 +0100

    fixed the visualization part

--- visualize_toilet.py
+++ visualize_toilet.py
@@ -18,13 +18,13 @@ from tkinter import filedialog
 from pandas import DataFrame
 
 
-#Below script for toilet and/or outing
+#Below script for toilet activity
 
 log_entry = 'rule-toilet.csv'
 hole_csv = 'days-toilet_failure.csv'
 out_file = 'out.csv'
 result_file = 'out.csv'
-graph_title = 'Visiting the toilet Ms. Dupont'
+graph_title = 'Visiting the toilet activity'
 label_y1 = 'Average duration in seconds'
 label_y2 = 'Number of passage per month'
 figure = 'toilet.pdf'
@@ -76,15 +76,15 @@ activity_dataset = pd.read_csv(out_file, delimiter =';', decimal=",",
                        names=["date", "sum", "mean", "sem"],
                        parse_dates=["date"], index_col="date")
 
-door_hole_1week = pd.read_csv(hole_csv, delimiter =';', decimal=","
-                              ,names=["date", "door_hole_1week"],
+toilet_hole = pd.read_csv(hole_csv, delimiter =';', decimal=","
+                              ,names=["date", "toilet_hole"],
                               parse_dates=["date"], index_col="date")
 
 
-door_hole_1week.index =  door_hole_1week.index.strftime("%Y-%m")
+toilet_hole.index =  toilet_hole.index.strftime("%Y-%m")
 
 #number of days where there was no failure
-#door_hole_1week = (30 - door_hole_1week)/35
+#toilet_hole = (30 - door_toilet)/35
 
 activity_dataset = activity_dataset.drop('date')
 activity_dataset = activity_dataset.loc[activity_dataset.index.dropna()]
@@ -100,7 +100,7 @@ activity_dataset = activity_dataset.reindex(idx, fill_value=0)
 
 activity_dataset.index = activity_dataset.index.strftime("%Y-%m")
 
-result = pd.concat([activity_dataset, door_hole_1week], axis=1, join_axes=[activity_dataset.index])
+result = pd.concat([activity_dataset, toilet_hole], axis=1, join_axes=[activity_dataset.index])
 
 
 result.index.rename('date', inplace=True)
@@ -110,7 +110,7 @@ export_csv = result.to_csv (result_file, index = True, header=False, sep = ';')
 result_activity = pd.read_csv(result_file, delimiter =';',
                          names=["date", "sum", "mean", "sem", "width"])
 result_activity = result_activity.fillna(0)
-result_activity['width'] = (30 - result_activity['width'])/35
+result_activity['width'] = (31 - result_activity['width'])/35
 
 #for exits devide the value by 60 to convert from seconds to minutes
 result_activity['mean'] = result_activity['mean'] #/60
@@ -119,10 +119,10 @@ result_activity['mean'] = result_activity['mean'] #/60
 
 result_activity.loc[result_activity['width'] == 0, 'on'] = 0
 result_activity.loc[result_activity['width'] > 0, 'on'] = -10
-
+plt.rcParams['figure.figsize'] = [9, 7]
 x = np.arange(12)
-plt.bar(x , result_activity['mean'], width=result_activity['width'], yerr=result_activity['sem']
-        , color="#1f77b4"
+plt.bar(x , result_activity['mean'], width=result_activity['width'], yerr=result_activity['sem'],
+         color="#1f77b4"
         )
 plt.bar(x, result_activity['on'], width=result_activity['width'], color='green')
 plt.gcf().autofmt_xdate()
@@ -137,8 +137,7 @@ plt.tick_params(axis="y", labelcolor="r", labelsize = 8)
 plt.plot(result_activity['sum'], "-r")
 plt.xticks(x, result_activity.date)
 
-
-
+sns.set_style("dark")
 result_activity
 #plt.savefig('/path/'+folder+'/'+figure)
 #plt.savefig('/path/toilet.pdf')
